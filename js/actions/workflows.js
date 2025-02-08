987442edb2f707705cc4b04b0993f519444397e8
liledix4 <liledix4@musician.org>
2025-02-08 12:17:42 +0000

    [2025.02.08]

--- js/actions/workflows.js
+++ js/actions/workflows.js
@@ -2,32 +2,32 @@ import { readTextFile } from "../../modules/js_xhr_ajax/xhr_ajax.js";
 
 
 export function getWorkflows(user, repo, token, callback) {
-    readTextFile(
-        {
-            method: 'GET',
-            url: `https://api.github.com/repos/${user}/${repo}/actions/runs?per_page=100`,
-            headers: [
-                {name: 'Accept', value: 'application/vnd.github+json'},
-                {name: 'Authorization', value: 'Bearer ' + token},
-                {name: 'X-GitHub-Api-Version', value: '2022-11-28'}
-            ]
-        },
-        callback
-    );
+  readTextFile(
+    {
+      method: 'GET',
+      url: `https://api.github.com/repos/${user}/${repo}/actions/runs?per_page=100`,
+      headers: [
+        {name: 'Accept', value: 'application/vnd.github+json'},
+        {name: 'Authorization', value: 'Bearer ' + token},
+        {name: 'X-GitHub-Api-Version', value: '2022-11-28'}
+      ]
+    },
+    callback
+  );
 }
 
 
 export function deleteWorkflows(user, repo, id, token, callback) {
-    readTextFile(
-        {
-            method: 'DELETE',
-            url: `https://api.github.com/repos/${user}/${repo}/actions/runs/${id}`,
-            headers: [
-                {name: 'Accept', value: 'application/vnd.github+json'},
-                {name: 'Authorization', value: 'Bearer ' + token},
-                {name: 'X-GitHub-Api-Version', value: '2022-11-28'}
-            ]
-        },
-        callback
-    );
+  readTextFile(
+    {
+      method: 'DELETE',
+      url: `https://api.github.com/repos/${user}/${repo}/actions/runs/${id}`,
+      headers: [
+        {name: 'Accept', value: 'application/vnd.github+json'},
+        {name: 'Authorization', value: 'Bearer ' + token},
+        {name: 'X-GitHub-Api-Version', value: '2022-11-28'}
+      ]
+    },
+    callback
+  );
 }
--- js/main.js
+++ js/main.js
@@ -1,58 +1,58 @@
 const types = [
-    'Actions',
-    'Activity',
-    'Apps',
-    'Billing',
-    'Branches',
-    'Checks',
-    'Classroom',
-    'Code scanning',
-    'Code security settings',
-    'Codes of conduct',
-    'Codespaces',
-    'Collaborators',
-    'Commits',
-    'Copilot',
-    'Dependabot',
-    'Dependency graph',
-    'Deploy keys',
-    'Deployments',
-    'Emojis',
-    'Gists',
-    'Git database',
-    'Gitignore',
-    'Interactions',
-    'Issues',
-    'Licenses',
-    'Markdown',
-    'Meta',
-    'Metrics',
-    'Migrations',
-    'Organizations',
-    'Packages',
-    'Pages',
-    'Private registries',
-    'Projects (classic)',
-    'Pull requests',
-    'Rate limit',
-    'Reactions',
-    'Releases',
-    'Repositories',
-    'Search',
-    'Secret scanning',
-    'Security advisories',
-    'Teams',
-    'Users'
+  'Actions',
+  'Activity',
+  'Apps',
+  'Billing',
+  'Branches',
+  'Checks',
+  'Classroom',
+  'Code scanning',
+  'Code security settings',
+  'Codes of conduct',
+  'Codespaces',
+  'Collaborators',
+  'Commits',
+  'Copilot',
+  'Dependabot',
+  'Dependency graph',
+  'Deploy keys',
+  'Deployments',
+  'Emojis',
+  'Gists',
+  'Git database',
+  'Gitignore',
+  'Interactions',
+  'Issues',
+  'Licenses',
+  'Markdown',
+  'Meta',
+  'Metrics',
+  'Migrations',
+  'Organizations',
+  'Packages',
+  'Pages',
+  'Private registries',
+  'Projects (classic)',
+  'Pull requests',
+  'Rate limit',
+  'Reactions',
+  'Releases',
+  'Repositories',
+  'Search',
+  'Secret scanning',
+  'Security advisories',
+  'Teams',
+  'Users'
 ];
 const selectors = {
-    types: document.getElementById('type'),
+  types: document.getElementById('type'),
 };
 
 
 types.forEach(type => {
-    const newOption = document.createElement('option');
-    const optionContent = document.createTextNode(type);
-    newOption.appendChild(optionContent);
-    newOption.setAttribute('value', type.toLowerCase().replaceAll(/[^0-9A-Za-z]+/g, '_').replaceAll(/^[^0-9A-Za-z]+|[^0-9A-Za-z]+$/g, ''));
-    selectors.types.appendChild(newOption);
+  const newOption = document.createElement('option');
+  const optionContent = document.createTextNode(type);
+  newOption.appendChild(optionContent);
+  newOption.setAttribute('value', type.toLowerCase().replaceAll(/[^0-9A-Za-z]+/g, '_').replaceAll(/^[^0-9A-Za-z]+|[^0-9A-Za-z]+$/g, ''));
+  selectors.types.appendChild(newOption);
 });
